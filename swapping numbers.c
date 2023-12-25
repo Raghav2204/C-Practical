d64bd683b9456aaaf3433afffd989a15ed819aea
Raghav Aggarwal <149301513+Raghav2204@users.noreply.github.com>
2023-12-25 17:34:06 +0530

    Update swapping numbers.c

--- swapping numbers.c
+++ swapping numbers.c
@@ -1,26 +1,18 @@
 #include <stdio.h>
-
-
-
 int main() {
     int a ,b;
-    printf("the first number is:\n");
+    printf("the first number is: ");
     scanf("%d",&a);
-    printf("the second number is:\n");
+    printf("the second number is: ");
     scanf("%d",&b);
-
-
-
     swapn(&a,&b);
     printf("after swapping values of a and b\n");
-    printf("a is %d\n", a);
-    printf("b is %d\n", b);
-
+    printf("a = %d", a);
+    printf("b = %d", b);
     return 0;
 }
-
-
-void swapn(int i, int j) {
+void swapn(int i, int j) 
+{
     int temp = i;
     i = j;
     j = temp;
