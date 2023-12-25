a8927895120302e97ae0b6cc14f3f5f57a4b3caa
Raghav Aggarwal <149301513+Raghav2204@users.noreply.github.com>
2023-12-25 17:30:23 +0530

    Add files via upload

---
+++ Simple_interest.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int SI();
+int main()
+{
+    SI();
+}
+int SI(){
+    float p,r,t,si;
+    printf("enter principal amount: ");
+    scanf("%f",&p);
+     printf("enter rate of interest: ");
+    scanf("%f",&r);
+     printf("enter time period: ");
+    scanf("%f",&t);
+    si=(p*r*t)/100;
+    printf("the simple interest is:%.2f",si);
+    return 0;
+}
