b57ce5cb2aea2524f296a4d3d98c34bd10e98663
Raghav Aggarwal <149301513+Raghav2204@users.noreply.github.com>
2023-12-25 17:26:05 +0530

    Update switch case.c

--- switch case.c
+++ switch case.c
@@ -1,21 +1,32 @@
 #include<stdio.h>
+#include<math.h>
 int main(){
     char op;
-printf("enter operation %c",op);
-scanf("%C",&op);
-int a,b;
-printf("enter the number");
-scanf("%d %d", &a,&b);// enter same character in the input as written in scan
+    printf("enter operation: ");
+    scanf("%c",&op);
+    int a,b;
+    printf("enter the numbers: ");
+    scanf("%d,%d", &a,&b);
 
-switch (op)
-{
-    case '+':// in case of character use inverted comma  for cases but not in integer
-    printf("sum is :%d",a+b);
-    break;
-    case'-':
-    printf
-    ("difference is: %d",a-b);
-    break;
-}
-return 0;
+    switch (op)
+    {
+        case '+':
+        printf("sum is: %d\n",a+b);
+        break;
+        case'-':
+        printf("difference is: %d\n",a-b);
+        break;
+        case'/':
+        printf("quotient is: %f\n",a/b);
+        break;
+        case'*':
+        printf("product is: %f\n",a*b);
+        break;
+        case'p':
+        printf("power is: %f\n",pow(a,b));
+        break;
+        default:
+        printf("enter valid operation\n");
+    }
+    return 0;
 }
