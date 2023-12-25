96ad7b2ae46add2018919d0532ae5aeac6f19591
Raghav Aggarwal <149301513+Raghav2204@users.noreply.github.com>
2023-12-25 17:56:24 +0530

    Add files via upload

---
+++ test.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    FILE *fp;
+    char c;
+    fp=fopen("test.txt","r");
+    if(fp==NULL){
+        printf("file not found");
+    }
+    while((c=fgetc(fp))!=EOF){
+
+        putchar(c);
+    }
+    fclose(fp);
+}
